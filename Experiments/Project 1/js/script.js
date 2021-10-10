b10b4bb9154250fdc664613acac40c15cd41901c
MGL <maxleger1999@gmail.com>
2021-10-10 17:05:12 -0400

    Started project 1

---
+++ Experiments/Project 1/js/script.js
@@ -0,0 +1,20 @@
+/**
+P5 project 1
+MGL
+
+Template Templat Template
+*/
+
+// setup ()
+//
+// Description of setup() goes here.
+function setup(): {
+ background(300, 0, 600);
+}
+
+// draw()
+//
+//Description of draw() goes here.
+function draw() {
+
+}
