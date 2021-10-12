0b9812ff2a73fe0d9665f4887ea756347dbd1c2f
MGL <maxleger1999@gmail.com>
2021-10-12 18:32:47 -0400

    Add New folder, modify the experiment folder

---
+++ Activities/Alien/js/script.js
@@ -0,0 +1,38 @@
+/***********************************************
+Template p5 project
+MGL
+
+This is a description of this template project.
+************************************************/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+  createCanvas(500, 500, WEBGL);
+
+  background (216, 216, 216);
+
+
+  }
+
+
+
+/**
+Description of draw()
+*/
+function draw() {
+  
+}
