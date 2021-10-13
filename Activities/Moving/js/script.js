e1c741c927a03ee858dbfbe2064ea16c63357818
MGL <maxleger1999@gmail.com>
2021-10-13 14:42:43 -0400

    Project 2

---
+++ Activities/Moving/js/script.js
@@ -0,0 +1,55 @@
+/***********************************************
+Template p5 project #2
+MGL
+
+This is a description of this template project.
+************************************************/
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+
+};
+
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  fill: 255,
+  alpha: 200
+
+};
+
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 75,
+  fill: 255,
+  alpha: 200
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+  createCanvas(500, 500);
+  noStroke();
+
+
+  }
+
+/**
+Description of draw()
+*/
+function draw() {
+
+  background(bg.r, bg.g, bg.b);
+bg.r = bg.r +1
+
+// Left circle1
+
+}
