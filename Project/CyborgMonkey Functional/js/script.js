e3eb130971a21443e1c2e65479a672f9fc485a8e
MGL <maxleger1999@gmail.com>
2021-10-21 06:18:36 -0400

    Uploaded CyborgMonkey functional and full version

---
+++ Project/CyborgMonkey Functional/js/script.js
@@ -0,0 +1,110 @@
+/***********************************************
+Template p5 project #2
+MGL
+This is a description of this template project.
+************************************************/
+
+ // Monkey image
+let cyberMonkeyImage;
+
+function preload() {
+  cyborgMonkeyImage = loadImage("assets/images/CyborgMonkey.png")
+
+}
+
+let circle1 = {
+  x: 51,
+  y: 51,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 3
+
+};
+
+let circle2 = {
+  x: 51,
+  y: 449,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 3
+
+};
+
+  let circle3 = {
+  x: 449,
+  y: 51,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 3
+
+};
+
+let circle4 = {
+  x: 449,
+  y: 449,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 3
+
+};
+
+// let zapImage;
+//
+// function preload() {
+//     zapImage = loadImage("assets/images/ZAP.png")
+
+
+
+function setup() {
+
+  createCanvas(500, 500);
+
+  circle1.x = width / 13.5;
+  circle2.x = width / 13.5
+  circle3.x = 12 * width / 13.5
+  circle4.x = 12 * width / 13.5
+
+  // Start moving the circles
+  circle1.vx = random(0,4);
+    circle2.vx = random(0,4);
+    circle3.vx = random(-4,0);
+    circle4.vx = random(-4,0);
+    circle1.vy = random(0,4);
+    circle2.vy = random(-4,0);
+    circle3.vy = random(0,4);
+    circle4.vy = random(-4.0);
+
+}
+
+function draw() {
+  background(0);
+
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+
+  circle3.x = circle3.x + circle3.vx;
+  circle3.y = circle3.y + circle3.vy;
+
+  circle4.x = circle4.x + circle4.vx;
+  circle4.y = circle4.y + circle4.vy;
+
+  ellipse(circle1.x, circle1.y, circle1.size)
+  ellipse(circle2.x, circle2.y, circle2.size)
+  ellipse(circle3.x, circle3.y, circle3.size)
+  ellipse(circle4.x, circle4.y, circle4.size)
+
+
+  // The Ellispse is an Actor for collision
+    ellipse(mouseX, mouseY, 81);
+
+    imageMode(CENTER)
+    image(cyborgMonkeyImage, mouseX, mouseY,0,0);
+
+}
--- Project/CyborgMonkey/js/script.js
+++ Project/CyborgMonkey/js/script.js
@@ -13,12 +13,26 @@ function preload() {
 
 }
 
+let titleCardImage;
+
+function preload() {
+  titleCardImage = loadImage("assets/images/titlecard.png")
+
+}
+
+let sleepImage;
+
+function preload() {
+  sleepImage = loadImage("assets/images/Sleep.png")
+
 let circle1 = {
   x: 51,
   y: 51,
   size: 50,
   vx: 0,
   vy: 0,
+  ax: 0,
+  ay: 0,
   speed: 3
 
 };
@@ -29,6 +43,8 @@ let circle2 = {
   size: 50,
   vx: 0,
   vy: 0,
+  ax: 0,
+  ay: 0,
   speed: 3
 
 };
@@ -39,6 +55,8 @@ let circle2 = {
   size: 50,
   vx: 0,
   vy: 0,
+  ax: 0,
+  ay: 0,
   speed: 3
 
 };
@@ -49,6 +67,8 @@ let circle4 = {
   size: 50,
   vx: 0,
   vy: 0,
+  ax: 0,
+  ay: 0,
   speed: 3
 
 };
@@ -58,35 +78,69 @@ let circle4 = {
 // function preload() {
 //     zapImage = loadImage("assets/images/ZAP.png")
 
-
+let state = 'title';
 
 function setup() {
 
   createCanvas(500, 500);
 
-}
+    circle1.x = width / 13.5;
+    circle2.x = width / 13.5
+    circle3.x = 12 * width / 13.5
+    circle4.x = 12 * width / 13.5
 
-function setupCircles() {
-  // circle1.x = width / 13.5;
-  // circle2.x = 12 * width / 13.5
-  // circle3.x = width / 13.5;
-  // circle4.x = 12 * width / 13.5
-
-  // Start moving the circles
-  circle1.vx = random(-circle1.speed, circle1.speed);
-  circle2.vx = random(-circle2.speed, circle2.speed);
-  circle3.vx = random(-circle3.speed, circle3.speed);
-  circle4.vx = random(-circle4.speed, circle4.speed);
-  circle1.vy = random(-circle1.speed, circle1.speed);
-  circle2.vy = random(-circle2.speed, circle2.speed);
-  circle3.vy = random(-circle3.speed, circle3.speed);
-  circle4.vy = random(-circle4.speed, circle4.speed);
+    // Start moving the circles
+    circle1.vx = random(0,4);
+    circle2.vx = random(0,4);
+    circle3.vx = random(-4,0);
+    circle4.vx = random(-4,0);
+    circle1.vy = random(0,4);
+    circle2.vy = random(-4,0);
+    circle3.vy = random(0,4);
+    circle4.vy = random(-4.0);
 
 }
 
 function draw() {
   background(0);
 
+  if (state === 'title') {
+    title();
+
+  }
+
+  else if (state === 'simulation') {
+    simulation();
+  }
+
+  else if (state === 'gathered') {
+    gathered();
+
+  }
+
+  else if (state === 'sleep') {
+    sleep();
+
+  }
+
+  function title() {
+    imageMode(CENTER)
+    image(titleCardImage,250,250);
+
+  }
+
+  function simulation() {
+    move();
+    display();
+  }
+
+  function sleep(); {
+    imageMode(CENTER)
+    image(sleepImage,250,250);
+  }
+
+
+// Drawing the batteries
   circle1.x = circle1.x + circle1.vx;
   circle1.y = circle1.y + circle1.vy;
 
@@ -99,16 +153,55 @@ function draw() {
   circle4.x = circle4.x + circle4.vx;
   circle4.y = circle4.y + circle4.vy;
 
+
   ellipse(circle1.x, circle1.y, circle1.size)
   ellipse(circle2.x, circle2.y, circle2.size)
   ellipse(circle3.x, circle3.y, circle3.size)
   ellipse(circle4.x, circle4.y, circle4.size)
 
 
-  // The Ellispse is an Actor for collision
-    ellipse(mouseX, mouseY, 81);
+  }
 
-    imageMode(CENTER)
-    image(cyborgMonkeyImage, mouseX, mouseY,0,0);
+  function move() {
+    // Move circles
+    circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+
+    circle3.x = circle3.x + circle3.vx;
+    circle3.y = circle3.y + circle3.vy;
+
+    circle4.x = circle4.x + circle4.vx;
+    circle4.y = circle4.y + circle4.vy;
+  }
+
+function display() {
+  // Drawing the batteries
+
+    ellipse(circle1.x, circle1.y, circle1.size)
+    ellipse(circle2.x, circle2.y, circle2.size)
+    ellipse(circle3.x, circle3.y, circle3.size)
+    ellipse(circle4.x, circle4.y, circle4.size)
+
+
+    // The Ellispse is an Actor for collision
+      ellipse(mouseX, mouseY, 81);
+
+      imageMode(CENTER)
+      image(cyborgMonkeyImage, mouseX, mouseY,0,0);
+}
+
+function checkOffscreen() {
+  //Check if the circles have gone offscreen
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x > 0 |circle2.x > width || circle2.y < 0 || circle2.y > height || circle3.x < 0 || circle3.x > width || circle3.y < 0 || circle3.y > height || circle4.x < 0 || circle4.x > width || circle4.y < 0 || circle4.y > height) {
+     state = 'sleep'
+}
+
+function mousePressed() {
+  if (state === "title"){
+    state = 'simulation';
+  }
 
 }
