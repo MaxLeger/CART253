dbd45a73360e75526e490a85ac03da278904d253
MGL <maxleger1999@gmail.com>
2021-10-22 12:18:26 -0400

    Re-aligned CyborgMonkey Working

--- Project/CyborgMonkey working/js/script.js
+++ Project/CyborgMonkey working/js/script.js
@@ -4,13 +4,10 @@ MGL
 This is a description of this template project.
 ************************************************/
 
- // Monkey image
+// Monkey image
 let cyberMonkeyImage;
+let titleCardImage;
 
-function preload() {
-  cyborgMonkeyImage = loadImage("assets/images/CyborgMonkey.png")
-
-}
 
 let circle1 = {
   x: 51,
@@ -32,7 +29,7 @@ let circle2 = {
 
 };
 
-  let circle3 = {
+let circle3 = {
   x: 449,
   y: 51,
   size: 50,
@@ -57,10 +54,15 @@ let circle4 = {
 // function preload() {
 //     zapImage = loadImage("assets/images/ZAP.png")
 
+let state = `simulation`;
 
+function preload() {
+  titleCardImage = loadImage("assets/images/titlecard.png");
+  cyborgMonkeyImage = loadImage("assets/images/CyborgMonkey.png");
+  // Load other stuff
+}
 
 function setup() {
-
   createCanvas(500, 500);
 
   circle1.x = width / 13.5;
@@ -69,18 +71,32 @@ function setup() {
   circle4.x = 12 * width / 13.5
 
   // Start moving the circles
-  circle1.vx = random(0,4);
-    circle2.vx = random(0,4);
-    circle3.vx = random(-4,0);
-    circle4.vx = random(-4,0);
-    circle1.vy = random(0,4);
-    circle2.vy = random(-4,0);
-    circle3.vy = random(0,4);
-    circle4.vy = random(-4.0);
+  circle1.vx = random(0, 4);
+  circle2.vx = random(0, 4);
+  circle3.vx = random(-4, 0);
+  circle4.vx = random(-4, 0);
+  circle1.vy = random(0, 4);
+  circle2.vy = random(-4, 0);
+  circle3.vy = random(0, 4);
+  circle4.vy = random(-4.0);
 
 }
 
 function draw() {
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `simulation`) {
+    simulation();
+  }
+}
+
+function title() {
+  // Display the image of the title card
+  image(titleCardImage, 0, 0);
+}
+
+function simulation() {
   background(0);
 
   circle1.x = circle1.x + circle1.vx;
@@ -100,11 +116,9 @@ function draw() {
   ellipse(circle3.x, circle3.y, circle3.size)
   ellipse(circle4.x, circle4.y, circle4.size)
 
-
   // The Ellispse is an Actor for collision
-    ellipse(mouseX, mouseY, 81);
+  ellipse(mouseX, mouseY, 81);
 
-    imageMode(CENTER)
-    image(cyborgMonkeyImage, mouseX, mouseY,0,0);
-
+  imageMode(CENTER)
+  image(cyborgMonkeyImage, mouseX, mouseY, 0, 0);
 }
