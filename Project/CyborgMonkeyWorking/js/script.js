214df40308e004640dee6bdb76a54eae834cd090
MGL <maxleger1999@gmail.com>
2021-10-28 14:59:47 -0400

    This version

--- Project/CyborgMonkeyWorking/js/script.js
+++ Project/CyborgMonkeyWorking/js/script.js
@@ -70,7 +70,7 @@ function preload() {
   winImage = loadImage("assets/images/WorkingMonkey.png");
   zapImage = loadImage("assets/images/ZAP.png");
 
-// ** The sound functions are playing irregularly **
+  // ** The sound functions are playing irregularly **
 
   // winSFX = loadSound("assets/sounds/win.wav");
   chargeSFX = loadSound("assets/sounds/Charge.m4a");
@@ -100,176 +100,153 @@ function setup() {
 function draw() {
   if (state === `title`) {
     title();
-  }
-  else if (state === `simulation`) {
+  } else if (state === `simulation`) {
     simulation();
   }
 
   // ** This state is currently not running **
-  // else if (state === 'sleep') {
-  //   sleep();
-  // }
+  else if (state === 'sleep') {
+    sleep();
+  }
 
   else if (state === 'gathered') {
     win();
   }
 
-function win() {
-  image(winImage, 250, 250);
-  // ** canceled sound **
-  // winSFX.play();
-}
+  function win() {
+    image(winImage, 250, 250);
+    // ** canceled sound **
+    // winSFX.play();
+  }
 
-function title() {
-  // Display the image of the title card
-  image(titleCardImage, 0, 0);
-
-}
-
-//This fuction is currently not running
-// function sleep() {
-//   // Display sleep image
-//   //image(sleepImage, 250, 250);
-
-}
-
-function simulation() {
-  display();
-  checkOverlap1();
-  checkOverlap2();
-  checkOverlap3();
-  checkOverlap4();
-
-  //** canceled sound **
-  // chargeSound1();
-  // chargeSound2();
-  // chargeSound3();
-  // chargeSound4();
-
-  //** canceled function **
-  //checkOffscreen();
-  gathered();
-}
-
-
-function display() {
-  background(0);
-
-  circle1.x = circle1.x + circle1.vx;
-  circle1.y = circle1.y + circle1.vy;
-
-  circle2.x = circle2.x + circle2.vx;
-  circle2.y = circle2.y + circle2.vy;
-
-  circle3.x = circle3.x + circle3.vx;
-  circle3.y = circle3.y + circle3.vy;
-
-  circle4.x = circle4.x + circle4.vx;
-  circle4.y = circle4.y + circle4.vy;
-
-  if (circle1.collected === false) {
-
-    image(zapImage, circle1.x, circle1.y, circle1.size)
+  function title() {
+    // Display the image of the title card
+    image(titleCardImage, 0, 0);
 
   }
 
-  if (circle2.collected === false) {
-
-    image(zapImage, circle2.x, circle2.y, circle2.size)
-
-  }
-
-  if (circle3.collected === false) {
-
-    image(zapImage, circle3.x, circle3.y, circle3.size)
+  //This fuction is currently not running
+  function sleep() {
+    // Display sleep image
+    image(sleepImage, 250, 250);
 
   }
 
-  if (circle4.collected === false) {
+  function simulation() {
+    display();
+    checkOverlap1();
+    checkOverlap2();
+    checkOverlap3();
+    checkOverlap4();
 
-    image(zapImage, circle4.x, circle4.y, circle4.size)
+    //** canceled sound **
+    // chargeSound1();
+    // chargeSound2();
+    // chargeSound3();
+    // chargeSound4();
 
+    //** canceled function **
+    checkOffscreen();
+    gathered();
   }
 
-  // The Ellispse is an Actor for collision
-  ellipse(mouseX, mouseY, 81);
 
-  imageMode(CENTER)
-  image(cyborgMonkeyImage, mouseX, mouseY, 0, 0);
-}
+  function display() {
+    background(0);
 
-//Check if the circles are overlapping [CONCEPT]
-function checkOverlap1() {
-  let d = dist(circle1.x, circle1.y, mouseX, mouseY);
-  if (circle1.collected === false && d < circle1.size / 2 + cyborgMonkeyImage.width / 2) {
-     circle1.collected = true;
-     chargeSFX.play();
+    circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+
+    circle3.x = circle3.x + circle3.vx;
+    circle3.y = circle3.y + circle3.vy;
+
+    circle4.x = circle4.x + circle4.vx;
+    circle4.y = circle4.y + circle4.vy;
+
+    if (circle1.collected === false) {
+
+      image(zapImage, circle1.x, circle1.y, circle1.size)
+
+    }
+
+    if (circle2.collected === false) {
+
+      image(zapImage, circle2.x, circle2.y, circle2.size)
+
+    }
+
+    if (circle3.collected === false) {
+
+      image(zapImage, circle3.x, circle3.y, circle3.size)
+
+    }
+
+    if (circle4.collected === false) {
+
+      image(zapImage, circle4.x, circle4.y, circle4.size)
+
+    }
+
+    // The Ellispse is an Actor for collision
+    ellipse(mouseX, mouseY, 81);
+
+    imageMode(CENTER)
+    image(cyborgMonkeyImage, mouseX, mouseY, 0, 0);
   }
-}
 
-function checkOverlap2() {
-  let d = dist(circle2.x, circle2.y, mouseX, mouseY);
-  if (circle2.collected === false && d < circle2.size / 2 + cyborgMonkeyImage.width / 2) {
-     circle2.collected = true;
-     chargeSFX.play();
+  //Check if the circles are overlapping [CONCEPT]
+  function checkOverlap1() {
+    let d = dist(circle1.x, circle1.y, mouseX, mouseY);
+    if (circle1.collected === false && d < circle1.size / 2 + cyborgMonkeyImage.width / 2) {
+      circle1.collected = true;
+      chargeSFX.play();
+    }
   }
-}
 
-function checkOverlap3() {
-  let d = dist(circle3.x, circle3.y, mouseX, mouseY);
-  if (circle3.collected === false && d < circle3.size / 2 + cyborgMonkeyImage.width / 2) {
-     circle3.collected = true;
-     chargeSFX.play();
+  function checkOverlap2() {
+    let d = dist(circle2.x, circle2.y, mouseX, mouseY);
+    if (circle2.collected === false && d < circle2.size / 2 + cyborgMonkeyImage.width / 2) {
+      circle2.collected = true;
+      chargeSFX.play();
+    }
   }
-}
 
-function checkOverlap4() {
-  let d = dist(circle4.x, circle4.y, mouseX, mouseY);
-  if (circle4.collected === false && d < circle4.size / 2 + cyborgMonkeyImage.width / 2) {
-     circle4.collected = true;
-     chargeSFX.play();
+  function checkOverlap3() {
+    let d = dist(circle3.x, circle3.y, mouseX, mouseY);
+    if (circle3.collected === false && d < circle3.size / 2 + cyborgMonkeyImage.width / 2) {
+      circle3.collected = true;
+      chargeSFX.play();
+    }
   }
-}
 
-function mousePressed() {
-  if (state === "title") {
-    state = 'simulation';
+  function checkOverlap4() {
+    let d = dist(circle4.x, circle4.y, mouseX, mouseY);
+    if (circle4.collected === false && d < circle4.size / 2 + cyborgMonkeyImage.width / 2) {
+      circle4.collected = true;
+      chargeSFX.play();
+    }
   }
-}
 
-//This fuction is currently not running
-// //Verifies if an energy bolt hasn't been catch by the monkey actor.
-// function checkOffscreen() {
-//   //Check if the circles have gone offscreen
-//   if (circle1.x > 500 || circle1.y > 500) {
-//     state = 'sleep'
-//   }
-// }
-
-function gathered() {
-  if (circle1.collected === true && circle2.collected === true && circle3.collected === true && circle4.collected === true) {
-    state = 'gathered'
+  function mousePressed() {
+    if (state === "title") {
+      state = 'simulation';
+    }
   }
-}
 
-// // ** Sound effects functions canceled **
-// function chargeSound1() {
-//   if (circle1.collected === true) {
-//     chargeSFX.play()
-//   }
-// }
-// function chargeSound2() {
-//   if (circle2.collected === true) {
-//     chargeSFX.play()
-//   }
-// }
-// function chargeSound3() {
-//   if (circle3.collected === true) {
-//     chargeSFX.play()
-//   }
-// }
-// function chargeSound4() {
-//   if (circle4.collected === true) {
-//     chargeSFX.play()
-//   }
-// }
+  //This fuction is currently not running
+  // //Verifies if an energy bolt hasn't been catch by the monkey actor.
+  function checkOffscreen() {
+    //Check if the circles have gone offscreen
+    if (circle1.x > 500 || circle1.y > 500) {
+      state = 'sleep'
+    }
+  }
+
+  function gathered() {
+    if (circle1.collected === true && circle2.collected === true && circle3.collected === true && circle4.collected === true) {
+      state = 'gathered'
+    }
+  }
