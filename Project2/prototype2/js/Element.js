93e625b1b5b9b6cc17de0f99a7659177c6fa42ae
MGL <maxleger1999@gmail.com>
2021-12-02 15:13:35 -0500

    Main

--- Project2/prototype2/js/Element.js
+++ Project2/prototype2/js/Element.js
@@ -3,7 +3,7 @@ class Element {
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.size = 90;
+    this.size = 45;
     this.color = color;
     this.isBeingDragged = false;
     this.offsetX = 0;
--- Project2/prototype2/js/script.js
+++ Project2/prototype2/js/script.js
@@ -31,12 +31,12 @@ let botEyeImage = {
   image: undefined
 }
 
-let shape1 = undefined;
-let shape2 = undefined;
-let shape3 = undefined;
+let flame = undefined;
+let water = undefined;
+let egg = undefined;
 
 
-let state = `title`;
+let state = `test`;
 
 function preload() {
   introImage = loadImage("assets/images/INTRO.png");
@@ -54,9 +54,25 @@ function setup() {
   topEyeImage.vy = 3;
   botEyeImage.vy = -3;
 
-  shape1 = new Element(width / 4, height / 2, color(255,0,0));
-  shape2 = new Element(width / 4, height / 4, color(0,250,0));
-  shape3 = new Element(width / 4, 3 * height / 4, color(0,0,250));
+  //Collumn 1
+
+  flame = new Element(width / 12, height / 12, color(255,0,0));
+  water = new Element(width / 12, 3 * height / 12, color(0,0,250));
+  egg = new Element(width / 12, 5 * height / 12, color(0,250,0));
+  raspberrySeed = new Element(width / 12, 7 * height / 12, color(255,0,0));
+  cabbageSeed = new Element(width / 12, 9 * height / 12, color(255,0,0));
+  soil = new Element(width / 12, 11 * height / 12, color(255,0,0));
+
+  //Collumn 2
+
+  kerosene = new Element(3 * width / 12, height / 12, color(255,0,0));
+  friedEgg = new Element(3 * width / 12, 3 * height / 12, color(255,0,0));
+  boillingWater = new Element(3 * width / 12, 5 * height / 12, color(90,90,230));
+
+
+
+
+
 
   for (let i = 0; i < 72; i++) {
     let y = 5 + 4 * i;
@@ -77,48 +93,83 @@ function statemachine() {
   } else if (state === 'wake') {
     lab();
   } else if (state === 'test') {
-    if (shape1.active) {
-      shape1.handleDragging();
-      shape1.display();
+    if (flame.active) {
+      flame.handleDragging();
+      flame.display();
     }
 
-    if (shape2.active) {
-      shape2.handleDragging();
-      shape2.display();
+    if (water.active) {
+      water.handleDragging();
+      water.display();
     }
 
-    if (shape3.active) {
-      shape3.handleDragging();
-      shape3.display();
+    if (egg.active) {
+      egg.handleDragging();
+      egg.display();
     }
+
+    if (raspberrySeed.active) {
+      raspberrySeed.handleDragging();
+      raspberrySeed.display();
+    }
+
+    if (cabbageSeed.active) {
+      cabbageSeed.handleDragging();
+      cabbageSeed.display();
+    }
+
+    if (soil.active) {
+      soil.handleDragging();
+      soil.display();
+    }
+
+    if (kerosene.active) {
+      kerosene.handleDragging();
+      kerosene.display();
+    }
+
+    if (friedEgg.active) {
+      friedEgg.handleDragging();
+      friedEgg.display();
+    }
+
+    if (boillingWater.active) {
+      boillingWater.handleDragging();
+      boillingWater.display();
+    }
+
+
     // Draws the centreline
     drawBoundary();
 
-    if (shape1.x > width / 2 && shape2.x > width / 2 && !shape1.isBeingDragged && !shape2.isBeingDragged) {
+    if (flame.x > width / 2 && water.x > width / 2 && !flame.isBeingDragged && !water.isBeingDragged) {
       instantSfx.play();
 
-      shape1.active = false;
-      shape2.active = false;
-      shape1 = new Element(width / 4, height / 2, color(255,0,0));
-      shape2 = new Element(width / 4, height / 4, color(0,255,0));
+      flame.active = false;
+      water.active = false;
+      flame = new Element(width / 12, height / 12, color(255,0,0));
+      water = new Element(width / 12, 3 * height / 12, color(0,0,250));
+      //Makes:
+      boillingWater = new Element(3 * width / 12, 5 * height / 12, color(90,90,230));
     }
-    if (shape1.x > width / 2 && shape3.x > width / 2 && !shape1.isBeingDragged && !shape3.isBeingDragged) {
+    if (flame.x > width / 2 && egg.x > width / 2 && !flame.isBeingDragged && !egg.isBeingDragged) {
       instantSfx.play();
 
-      shape1.active = false;
-      shape3.active = false;
-      shape1 = new Element(width / 4, height / 2, color(255,0,0));
-      shape3 = new Element(width / 4, 3 * height / 4, color(0,0,250));
+      flame.active = false;
+      egg.active = false;
+      flame = new Element(width / 12, height / 12, color(255,0,0));
+      egg = new Element(width / 12, 5 * height / 12, color(0,250,0));
+      //Makes:
+      friedEgg = new Element(3 * width / 12, 3 * height / 12, color(255,0,0));
+
     }
-    if (shape2.x > width / 2 && shape3.x > width / 2 && !shape2.isBeingDragged && !shape3.isBeingDragged) {
+    if (water.x > width / 2 && egg.x > width / 2 && !water.isBeingDragged && !egg.isBeingDragged) {
       instantSfx.play();
 
-      shape2.active = false;
-      shape3.active = false;
-      shape2 = new Element(width / 4, height / 4, color(0,255,0));
-      shape3 = new Element(width / 4, 3 * height / 4, color(0,0,250));
-
-
+      water.active = false;
+      egg.active = false;
+      water = new Element(width / 12, 3 * height / 12, color(0,0,250));
+      egg = new Element(width / 12, 5 * height / 12, color(0,250,0));
     }
   }
 }
@@ -132,9 +183,16 @@ function mousePressed() {
     state = 'transition';
   }
   if (state === "test") {
-    shape1.mousePressed();
-    shape2.mousePressed();
-    shape3.mousePressed();
+    flame.mousePressed();
+    water.mousePressed();
+    egg.mousePressed();
+    raspberrySeed.mousePressed();
+    cabbageSeed.mousePressed();
+    soil.mousePressed();
+    kerosene.mousePressed();
+    friedEgg.mousePressed();
+    boillingWater.mousePressed();
+
   }
 }
 
@@ -176,9 +234,15 @@ function drawBoundary() {
 
 function mouseReleased() {
   if (state === "test") {
-    shape1.mouseReleased();
-    shape2.mouseReleased();
-    shape3.mouseReleased();
+    flame.mouseReleased();
+    water.mouseReleased();
+    egg.mouseReleased();
+    raspberrySeed.mouseReleased();
+    cabbageSeed.mouseReleased();
+    soil.mouseReleased();
+    kerosene.mouseReleased();
+    friedEgg.mouseReleased();
+    boillingWater.mouseReleased();
   }
 }
 
